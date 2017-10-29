20bc3af7ec947750fee6ef10f0acc71bea5d80be
robertbutacu <robert.butacu@yahoo.com>
2017-10-29 14:14:47 +0200

    added tree data structure explanatory comment

--- id3/src/dataset/Dataset.scala
+++ id3/src/dataset/Dataset.scala
@@ -1,3 +1,7 @@
 package dataset
 
 case class Dataset(attribute: String, data: List[String])
+
+case class Leaf(attribute: String)
+
+case class Node(attribute: String, leafs: List[Leaf], nodes: List[Node])
---
+++ id3/src/id3/Id3.scala
@@ -0,0 +1,19 @@
+package id3
+
+object Id3 {
+  /**
+    * Compute the entropy for each attribute
+    * Split the set into subsets using the attribute for each entropy is minimum ( <=> information gain is maximal )
+    * Make a decision Tree containing that attribute
+    * Recurse on subsets using remaining attributes
+    *   Entropy(S) = -p(I)log2(p(I))
+    *   InformationGain(S, A) = Entropy(S) - ((|Sv| \ |S|) * Entropy(Sv))
+    *   Sv = subset of S for which attribute A has value v
+    *   |Sv| = number of elements in Sv
+    *   |S|  = number of elements in S
+    */
+
+  def apply(): Unit = {
+
+  }
+}
