7c9c241c5482cc43c28374ef0d1db2ed43e69256
robertbutacu <robert.butacu@yahoo.com>
2017-10-30 20:15:58 +0200

    refactored Node

--- id3/src/dataset/Dataset.scala
+++ id3/src/dataset/Dataset.scala
@@ -4,4 +4,4 @@ case class Dataset(attribute: String, data: List[String])
 
 case class Leaf(attribute: String)
 
-case class Node(attribute: String, leafs: List[(String, Leaf)] = List.empty, nodes: List[(String, Node)] = List.empty)
+case class Node(attribute: String, leafs: List[Leaf] = List.empty, nodes: List[(String, Node)] = List.empty)
