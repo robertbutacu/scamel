c460113aa0e5fad00743a2f7e5f24f27a32bb592
robertbutacu <robert.butacu@yahoo.com>
2017-11-01 00:59:37 +0200

    refactoring

--- id3/src/id3/BestAttributeFinder.scala
+++ id3/src/id3/BestAttributeFinder.scala
@@ -29,18 +29,20 @@ object BestAttributeFinder {
 
     val bestAttributeIndex = informationGains.indexOf(informationGains.max)
 
-    val subtables = trainingData(bestAttributeIndex).data
+    val subsets = trainingData(bestAttributeIndex).data
       .distinct
-      .map{v => createSubtableFromRowValue(bestAttributeIndex, v, trainingData, conclusion)}
+      .map{v => createSubsetFromRowValue(bestAttributeIndex, v, trainingData, conclusion)}
 
     (
       trainingData(bestAttributeIndex).attribute,
-      subtables.map(t => (t._1(bestAttributeIndex).data.head, t._1.patch(bestAttributeIndex, Nil, 1), t._2))
+      subsets.map(t => (t._1(bestAttributeIndex).data.head, t._1.patch(bestAttributeIndex, Nil, 1), t._2))
     )
   }
 
 
-  /**
+  /** log(base e) x/log(base e) 2
+    *  Scala/Java doesn't have anything implemented for log of custom base ( but 10 and e )
+    *  manual conversion needed
     *
     * Represented by Sum( - P(i) * log2(P(i)) ).
     * Where P(i) => probability of i
@@ -52,31 +54,26 @@ object BestAttributeFinder {
     val probabilities = input
       .map(e => (e, input.count(b => b == e).toDouble / input.length.toDouble)).toSet
 
-    val entropy = probabilities.foldRight(0.0)((curr, acc) =>
-      // log(base e) x/log(base e) 2
-      // Scala/Java doesn't have anything implemented for log of custom base ( but 10 and e )
-      // manual conversion needed
-      acc - curr._2 * (log(curr._2) / log(2))
-    )
+    val entropy = probabilities.foldRight(0.0)((curr, acc) => acc - curr._2 * (log(curr._2) / log(2)))
 
     (entropy * 10000).floor / 10000
   }
 
 
   /** Why use zipWithIndex => a value from a given dataset will need to be filtered against the value
-    *   it is wanted to create the sub-table by. To do this, the index is needed - simple as that.
+    *   it is wanted to create the subset by. To do this, the index is needed - simple as that.
     *
     * @param columnIndex - index of column which will be checked for equality, needed to filter the data
     * @param rowValue - the value of the row which will be used to filter the data
     * @param trainingData - input dataset
     * @param conclusion - the conclusion for an input data
-    * @return a new table ( which is represented as a list of datasets ), and its inferred conclusion,
+    * @return a new table ( which is represented as a list of training data ), and its inferred conclusion,
     *         where each value of the column matching columnIndex is equal to rowValue
     */
-  def createSubtableFromRowValue(columnIndex: Int,
-                                 rowValue: String,
-                                 trainingData: List[Dataset],
-                                 conclusion: Dataset): (List[Dataset], Dataset) = {
+  def createSubsetFromRowValue(columnIndex: Int,
+                               rowValue: String,
+                               trainingData: List[Dataset],
+                               conclusion: Dataset): (List[Dataset], Dataset) = {
     (trainingData
       .map { e =>
         Dataset(e.attribute,
@@ -97,7 +94,7 @@ object BestAttributeFinder {
 
 
   /**
-    * In order to compute information gain, it is needed for sub-tables to be created for each unique dataset value
+    * In order to compute information gain, it is needed for subsets to be created for each unique dataset value
     * Example:
     * Female Bus
     * Female Bus
@@ -113,27 +110,27 @@ object BestAttributeFinder {
     * Man    Car
     * )
     * This way, it is easy to compute the entropy for each value and add it up to a information gain.
-    * @param table - a sub-table
-    * @return a list of tables where e._1 is unique in any of the elements
+    * @param trainingData - a sub-table
+    * @return a list of subsets where e._1 is unique in any of the elements
     */
-  private def splitIntoTables(table: List[(String, String)]): List[List[(String, String)]] = {
-    val (packed, next) = table.span(_._1 == table.head._1)
+  private def splitIntoSubsets(trainingData: List[(String, String)]): List[List[(String, String)]] = {
+    val (packed, next) = trainingData.span(_._1 == trainingData.head._1)
     if (next.isEmpty) List(packed)
-    else packed :: splitIntoTables(next)
+    else packed :: splitIntoSubsets(next)
   }
 
 
   /**
     *
-    * @param trainingData - the sub-table for which an entropy is required
+    * @param trainingData - the subset for which an entropy is required
     * @return the entropy for an attribute, which will be later require to compute information gain.
     */
   private def computeDatasetEntropy(trainingData: (Dataset, Dataset)): Entropy = {
     val rows = trainingData._1.data.indices.map(e => (trainingData._1.data(e), trainingData._2.data(e))).toList
 
-    val tables = splitIntoTables(rows.sortWith((e1, e2) => e1._1 < e2._1))
+    val tables = splitIntoSubsets(rows.sortWith((e1, e2) => e1._1 < e2._1))
 
-    tables.map(t => entropyForSubTable(t, rows.length)).sum
+    tables.map(t => entropyForSubset(t, rows.length)).sum
   }
 
 
@@ -141,6 +138,6 @@ object BestAttributeFinder {
     * Represented by - (numberOfElementsInSubtable/numberOfElementsInTable) * Entropy(Attribute)
     * The minus will be added by the apply function.
     */
-  private def entropyForSubTable(subTable: List[(String, String)], tableSize: Int): Entropy =
-    subTable.length.toDouble / tableSize.toDouble * computeEntropy(subTable.map(e => e._2))
+  private def entropyForSubset(subset: List[(String, String)], tableSize: Int): Entropy =
+    subset.length.toDouble / tableSize.toDouble * computeEntropy(subset.map(e => e._2))
 }
