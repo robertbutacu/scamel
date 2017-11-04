897ba6758b451206c8901b8df67a91925f744df3
robertbutacu <robert.butacu@yahoo.com>
2017-11-04 22:08:22 +0200

    indentation

--- id3/src/dataset/Dataset.scala
+++ id3/src/dataset/Dataset.scala
@@ -4,7 +4,7 @@ case class Dataset(attribute: String, data: List[String])
 
 case class BestAttribute(attribute: String, subsets: List[Subset])
 
-case class Subset(attribute: String,table: List[Dataset],conclusion: Dataset)
+case class Subset(attribute: String, table: List[Dataset], conclusion: Dataset)
 
 case class Leaf(attribute: String)
 
