44aac8786eb77d77693c24bff93bf7ab23e71b10
robertbutacu <robert.butacu@yahoo.com>
2017-11-11 00:05:27 +0200

    added data and class

--- naive-bayes-classifier/src/main/scala/Main.scala
+++ naive-bayes-classifier/src/main/scala/Main.scala
@@ -1,3 +1,18 @@
-object Main extends App{
-  println("Bayesian classifier")
+import bayes.classifier.{Dataset, NaiveBayes}
+
+object Main extends App {
+  NaiveBayes(
+    List(
+      Dataset("outlook", List("sunny", "sunny", "overcast", "rainy", "rainy", "rainy", "overcast",
+        "sunny", "sunny", "rainy", "sunny", "overcast", "overcast", "rainy")),
+      Dataset("temperature", List("hot", "hot", "hot", "mild", "cool", "cool", "cool", "mild", "cool", "mild",
+        "mild", "mild", "hot", "mild")),
+      Dataset("humidity", List("high", "high", "high", "high", "normal", "normal", "normal", "high",
+        "normal", "normal", "normal", "high", "normal", "high")),
+      Dataset("windy", List("false", "true", "false", "false", "false", "true", "true", "false", "false", "false",
+        "true", "true", "false", "true"))),
+    List(
+    ),
+    Dataset("play", List("no", "no", "yes", "yes", "yes", "no", "yes", "no", "yes", "yes", "yes", "yes", "yes", "no"))
+  )
 }
