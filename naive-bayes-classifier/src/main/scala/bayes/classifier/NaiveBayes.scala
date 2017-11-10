c321871990664336574cd9ee17f003546ef648b0
robertbutacu <robert.butacu@yahoo.com>
2017-11-10 23:39:28 +0200

    comments - whats NaiveBayes - basic scaffolding through comments

---
+++ naive-bayes-classifier/src/main/scala/bayes/classifier/NaiveBayes.scala
@@ -0,0 +1,27 @@
+package bayes.classifier
+
+
+/**
+  * Pro and Cons of Naive Bayes:
+  *     Pro:
+  *       - easy and fast to predict a class of test data set
+  *       - performs better compared to other models assuming independence
+  *
+  *     Cons:
+  *       - doesn't work on data not present in the training set
+  *           - can use other techniques to avoid this
+  *       - known as a bad estimator
+  *       - independent predictor assumption ( hard to achieve irl )
+  */
+
+/**
+  *  1. How many positive/negative in conclusion -> probability.
+  *  2. Individual probability for each attribute from the dataset -> count and probability
+  *  3. Given new data to classify, compute P(X | Conclusion = Positive )
+  *                                         P(X | Conclusion = Negative )
+  *  4. Divide both sides by evidence P(X) to normalize.
+  *         P(X) = Product Of Probability Of An Attribute to be equal to the Input Data's Attribute Value.
+  */
+object NaiveBayes {
+
+}
