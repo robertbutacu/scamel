cf674b07b7fbe7881b87ccf2bcd4dc4f2ffc5c0b
robertbutacu <robert.butacu@yahoo.com>
2017-11-15 19:00:06 +0200

    renaming

--- clustering/src/main/scala/Hierarchical/Agglomerative/Clustering/Cluster.scala
+++ clustering/src/main/scala/Hierarchical/Agglomerative/Clustering/Cluster.scala
@@ -1,5 +1,5 @@
 package Hierarchical.Agglomerative.Clustering
 
-case class Node(connection: Connection, distance: Double, creationIndex: Int)
+case class Node(connection: Connection, coordinates: Point, creationIndex: Int)
 
 case class Connection(first: Either[Point, Node], second: Either[Point, Node])
