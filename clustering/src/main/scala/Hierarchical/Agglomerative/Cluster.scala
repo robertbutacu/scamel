67a65984fb63eed1eb005a9028e10507c5fe82ce
robertbutacu <robert.butacu@yahoo.com>
2017-12-18 21:36:05 +0200

    added Cluster attribute

--- clustering/src/main/scala/Hierarchical/Agglomerative/Cluster.scala
+++ clustering/src/main/scala/Hierarchical/Agglomerative/Cluster.scala
@@ -3,6 +3,7 @@ package Hierarchical.Agglomerative
 case class Point(X: Double, Y: Double)
 
 case class Cluster(points: List[Point],
+                   creationIndex: Int = 0,
                    leftCluster: Option[Cluster] = None,
                    rightCluster: Option[Cluster] = None) {
   val name: String = points.reduce(_.toString ++ _.toString)
