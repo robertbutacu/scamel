cbc9739ba1499de034c193334ae555649bcae315
robertbutacu <robert.butacu@yahoo.com>
2017-12-18 20:40:52 +0200

    SingleLinkage

--- clustering/src/main/scala/Hierarchical/Agglomerative/clustering/types/SingleLinkage.scala
+++ clustering/src/main/scala/Hierarchical/Agglomerative/clustering/types/SingleLinkage.scala
@@ -1,6 +1,28 @@
 package Hierarchical.Agglomerative.clustering.types
-import Hierarchical.Agglomerative.Cluster
+
+import Hierarchical.Agglomerative.{Cluster, Point}
 
 case object SingleLinkage extends Method {
-  override def formCluster(clusters: List[Cluster]): Unit = ???
+  override def formCluster(clusters: List[Cluster]): (Cluster, Cluster, Double) = {
+    //the formula for the distance between 2 points is sqrt( ( x2 - x1) ^ 2 + (y2 - y1) ^ 2 )
+    def shortestDistance(current: Cluster, other: Cluster): Double = {
+      def distanceBetweenPoints(current: Point, other: Point): Double = {
+        Math.sqrt(Math.pow(current.X - other.Y, 2) + Math.pow(current.Y - other.Y, 2))
+      }
+
+      val distanceBetweenAllPoints = for {
+        currentPoint <- current.points
+        otherPoint <- other.points
+      } yield distanceBetweenPoints(currentPoint, otherPoint)
+
+      distanceBetweenAllPoints.min
+    }
+
+    val shortestDistancesBetweenClusters = for {
+      currentCluster <- clusters
+      otherCluster <- clusters.filterNot(_ == currentCluster)
+    } yield (currentCluster, otherCluster, shortestDistance(currentCluster, otherCluster))
+
+    shortestDistancesBetweenClusters.minBy(_._3)
+  }
 }
