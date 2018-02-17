52b912b00aa46e556c29e1858417307c9e2b8bdd
robertbutacu <robert.butacu@yahoo.com>
2018-02-17 17:41:48 +0200

    added some comments

--- k-means/src/main/scala/kmeans/KMeans.scala
+++ k-means/src/main/scala/kmeans/KMeans.scala
@@ -35,15 +35,27 @@ object KMeans {
   }
 
   private def createClusters(points: List[Point], centroids: List[Centroid]): List[Cluster] = {
+    //compute , for each point, the distance to each centroid
     val distancesToCentroids = for {
       point <- points
       distance <- centroids.map(c => DistanceToCentroid(point, c))
     } yield distance
 
+    //now obtain the shortest distance
     val minDistances = splitPoints(distancesToCentroids).map(e => e.minBy(_.distance))
 
+    //sorting by centroids' names so it's easier to group
     val sortedDistances = minDistances.sortBy(c => c.centroid.name)
 
+    //now create clusters as the list is ordered by centroids
+    /*
+      For example, this:
+        DistanceToCentroid(point, Centroid1, X)
+        DistanceToCentroid(point, Centroid1, X)
+        DistanceToCentroid(point, Centroid2, X)
+
+      will become 2 clusters
+     */
     //TODO some centroids are left behind
     splitIntoClusters(sortedDistances).map(c => Cluster(c.head.centroid, c.map(o => o.point)))
   }
