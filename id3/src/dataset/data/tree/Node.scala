90c93240b9397692b5cc94162e183a1b9ee56e96
robertbutacu <robert.butacu@yahoo.com>
2018-02-18 13:41:51 +0200

    added Id3Classifier todo node

--- id3/src/dataset/data/tree/Node.scala
+++ id3/src/dataset/data/tree/Node.scala
@@ -1,5 +1,11 @@
 package dataset.data.tree
 
+
+//TODO refactor so that the nodes do not represent data too, only attributes
 case class Node(attribute: String,
                 nodes: List[Node] = List.empty,
                 leafs: List[Leaf] = List.empty)
+
+object Node {
+
+}
---
+++ id3/src/id3/Id3Classifier.scala
@@ -0,0 +1,9 @@
+package id3
+
+import dataset.data.tree.Node
+
+object Id3Classifier {
+  def classify(id3Tree: Node, toClassify: Map[String, String]): Option[String] = {
+    None
+  }
+}
