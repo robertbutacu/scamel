434cf8f75aee9ff6a7d6ee0da1c1dc1990623c12
robertbutacu <robert.butacu@yahoo.com>
2018-03-31 22:38:03 +0300

    indentation and renaming

--- clustering/src/main/scala/Main.scala
+++ clustering/src/main/scala/Main.scala
@@ -33,5 +33,5 @@ object Main extends App {
   ClusteringAlgorithm.clusterize(points, CompleteLinkage)(DistanceImplicits.manhattanDistance)
     .prettyPrinter()*/
 
-  println(DistanceCalculator.computeDistance(BidimensionalPoint(4, 5), BidimensionalPoint(5, 5), EuclideanDistance))
+  println(Calculator.computeDistance(BidimensionalPoint(4, 5), BidimensionalPoint(5, 5), EuclideanDistance))
 }
