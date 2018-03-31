80ec3c95719a5481359aa301091c769b248158c6
robertbutacu <robert.butacu@yahoo.com>
2018-03-31 23:35:12 +0300

    renaming

--- clustering/src/main/scala/Hierarchical/Agglomerative/clustering/dimensions/Distance.scala
+++ clustering/src/main/scala/Hierarchical/Agglomerative/clustering/dimensions/Distance.scala
@@ -1,60 +1,55 @@
 package Hierarchical.Agglomerative.clustering.dimensions
 
 trait Distance[A, P[_], D <: DistanceType] {
-  def computeDistance(from: P[A], to: P[A], distanceType: D): A
+  def computeDistance(A: P[A], B: P[A], distanceType: D): A
 }
 
 object Distance {
   implicit def euclidean1D: Distance[Double, UnidimensionalPoint, EuclideanDistance.type] =
-    (from: UnidimensionalPoint[Double], to: UnidimensionalPoint[Double], _: EuclideanDistance.type) => {
-      //distance.computeDistance(from, to)
-      //for now, just the euclidean distance
-
-
+    (A: UnidimensionalPoint[Double], B: UnidimensionalPoint[Double], _: EuclideanDistance.type) => {
       // | |X| - |Y| |
-      0.0
+      Math.abs(Math.abs(A.X) - Math.abs(B.X))
     }
 
   implicit def euclidean2D: Distance[Double, BidimensionalPoint, EuclideanDistance.type] =
-    (from: BidimensionalPoint[Double], to: BidimensionalPoint[Double], _: EuclideanDistance.type) => {
+    (A: BidimensionalPoint[Double], B: BidimensionalPoint[Double], _: EuclideanDistance.type) => {
       val num = implicitly[Numeric[Double]]
 
-      //Math.sqrt(Math.pow(A.X - B.X, 2) + Math.pow(A.Y - B.Y, 2))
-      num.zero
+      Math.sqrt(Math.pow(A.X - B.X, 2) + Math.pow(A.Y - B.Y, 2))
     }
 
   implicit def euclidean3D: Distance[Double, TridimensionalPoint, EuclideanDistance.type] =
-    (from: TridimensionalPoint[Double], to: TridimensionalPoint[Double], _: EuclideanDistance.type) => {
+    (A: TridimensionalPoint[Double], B: TridimensionalPoint[Double], _: EuclideanDistance.type) => {
       implicitly[Numeric[Double]].zero
     }
 
   implicit def manhattan1D: Distance[Double, UnidimensionalPoint, ManhattanDistance.type] =
-    (from: UnidimensionalPoint[Double], to: UnidimensionalPoint[Double], _: ManhattanDistance.type) => {
+    (A: UnidimensionalPoint[Double], B: UnidimensionalPoint[Double], _: ManhattanDistance.type) => {
       implicitly[Numeric[Double]].zero
     }
 
   implicit def manhattan2D: Distance[Double, BidimensionalPoint, ManhattanDistance.type] =
-    (from: BidimensionalPoint[Double], to: BidimensionalPoint[Double], _: ManhattanDistance.type) => {
+    (A: BidimensionalPoint[Double], B: BidimensionalPoint[Double], _: ManhattanDistance.type) => {
       implicitly[Numeric[Double]].zero
     }
 
   implicit def manhattan3D: Distance[Double, TridimensionalPoint, ManhattanDistance.type] =
-    (from: TridimensionalPoint[Double], to: TridimensionalPoint[Double], _: ManhattanDistance.type) => {
+    (A: TridimensionalPoint[Double], B: TridimensionalPoint[Double], _: ManhattanDistance.type) => {
       implicitly[Numeric[Double]].zero
     }
 
   implicit def chebyshev1D: Distance[Double, UnidimensionalPoint, ChebyshevDistance.type] =
-    (from: UnidimensionalPoint[Double], to: UnidimensionalPoint[Double], _: ChebyshevDistance.type) => {
+    (A: UnidimensionalPoint[Double], B: UnidimensionalPoint[Double], _: ChebyshevDistance.type) => {
       implicitly[Numeric[Double]].zero
     }
 
   implicit def chebyshev2D[A: Numeric]: Distance[A, BidimensionalPoint, ChebyshevDistance.type] =
-    (from: BidimensionalPoint[A], to: BidimensionalPoint[A], _: ChebyshevDistance.type) => {
+    (A: BidimensionalPoint[A], B: BidimensionalPoint[A], _: ChebyshevDistance.type) => {
       implicitly[Numeric[A]].zero
     }
 
   implicit def chebyshev3D[A: Numeric]: Distance[A, TridimensionalPoint, ChebyshevDistance.type] =
-    (from: TridimensionalPoint[A], to: TridimensionalPoint[A], _: ChebyshevDistance.type) => {
+    (A: TridimensionalPoint[A], B: TridimensionalPoint[A], _: ChebyshevDistance.type) => {
       implicitly[Numeric[A]].zero
     }
 }
