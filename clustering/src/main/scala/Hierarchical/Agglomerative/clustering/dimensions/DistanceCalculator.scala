8878df183d7a3e8a63d90987c7ae0ed988c1adf3
robertbutacu <robert.butacu@yahoo.com>
2018-03-31 21:30:23 +0300

    now works

--- clustering/src/main/scala/Hierarchical/Agglomerative/clustering/dimensions/DistanceCalculator.scala
+++ clustering/src/main/scala/Hierarchical/Agglomerative/clustering/dimensions/DistanceCalculator.scala
@@ -1,7 +1,7 @@
 package Hierarchical.Agglomerative.clustering.dimensions
 
 object DistanceCalculator {
-  def computeDistance[A: Numeric, B[_]: Point[A]](from: B[A], to: B[A])(implicit dimension: Dimension[A, B]): A = {
+  def computeDistance[A: Numeric, B[_] <: Point[_]](from: B[A], to: B[A])(implicit dimension: Dimension[A, B]): A = {
     dimension.computeDistance(from, to)
   }
 }
