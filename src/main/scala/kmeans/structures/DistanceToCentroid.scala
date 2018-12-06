e9a68c8dc5869518905ffc97594fce64c92e5408
robertbutacu <robertbutacu@gmail.com>
2018-12-06 10:31:25 +0000

    migrated to N-dimensions K-Means algorithm

--- src/main/scala/kmeans/structures/DistanceToCentroid.scala
+++ src/main/scala/kmeans/structures/DistanceToCentroid.scala
@@ -2,7 +2,7 @@ package kmeans.structures
 
 import Hierarchical.Agglomerative.clustering.dimensions.{Distance, EuclideanDistance}
 
-case class DistanceToCentroid[P[_], A](point: P, centroid: Centroid[P, A], distance: A)
+case class DistanceToCentroid[P[_], A](point: P[A], centroid: Centroid[P, A], distance: A)
 
 object DistanceToCentroid {
   def createCentroid[P[_], A](p: P[A], c: Centroid[P, A])(implicit distance: Distance[A, P, EuclideanDistance.type]): DistanceToCentroid[P, A] = {
