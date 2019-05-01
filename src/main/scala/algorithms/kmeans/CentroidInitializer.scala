6478c91811e7c779f6cb4f9be16cc1a80fac1aad
robertbutacu <17516693+robertbutacu@users.noreply.github.com>
2019-05-01 13:57:58 +0100

    CentroidCalculator implicits

--- src/main/scala/algorithms/kmeans/CentroidInitializer.scala
+++ src/main/scala/algorithms/kmeans/CentroidInitializer.scala
@@ -1,7 +1,6 @@
 package algorithms.kmeans
 
-import algorithms.kmeans.KMeans.Coordinate
-import common.data.BidimensionalPoint
+import common.data.{BidimensionalPoint, TridimensionalPoint, UnidimensionalPoint}
 import data.kmeans.structures.Centroid
 
 import scala.language.higherKinds
@@ -12,43 +11,30 @@ trait CentroidInitializer[P[_]] {
 }
 
 object CentroidInitializer {
-  val bidimensionalRandomInitializer: CentroidInitializer[BidimensionalPoint] = new CentroidInitializer[BidimensionalPoint] {
-    def initialize[A](points: List[BidimensionalPoint[A]], number: Int)(implicit frac: Fractional[A]): List[Centroid[BidimensionalPoint, A]] = {
-      val min: Coordinate[A] = Coordinate(points.minBy(_.X).X, points.minBy(_.Y).Y)
-
-      val max: Coordinate[A] = Coordinate(points.maxBy(_.X).X, points.maxBy(_.Y).Y)
-
+  val tridimensionalRandomInitializer: CentroidInitializer[TridimensionalPoint] = new CentroidInitializer[TridimensionalPoint] {
+    override def initialize[A](points: List[TridimensionalPoint[A]], number: Int)(implicit frac: Fractional[A]): List[Centroid[TridimensionalPoint, A]] = {
       (1 to number)
         .map { p =>
-         /* println(frac.toInt(frac.plus(frac.minus(max.X, min.X), min.X)))
-         Centroid("Centroid " + p,
-            //TODO this is shit -> maybe write an own typeclass which converts to A ?
-            BidimensionalPoint("Point " + p,
-              frac.fromInt(Random.nextDouble(frac.toDouble(frac.plus(frac.minus(max.X, min.X), min.X)))),
-              frac.fromInt(Random.nextInt(frac.toInt(frac.plus(frac.minus(max.Y, min.Y), min.Y))))
-            ))*/
+          Centroid("Centroid " + p, points(Random.nextInt(points.length)))
+        }.toList
+    }
+  }
+
+  val bidimensionalRandomInitializer: CentroidInitializer[BidimensionalPoint] = new CentroidInitializer[BidimensionalPoint] {
+    def initialize[A](points: List[BidimensionalPoint[A]], number: Int)(implicit frac: Fractional[A]): List[Centroid[BidimensionalPoint, A]] = {
+      (1 to number)
+        .map { p =>
          Centroid("Centroid " + p, points(Random.nextInt(points.length)))
         }.toList
     }
   }
 
-  val kmeansBidimensionalInitializer: CentroidInitializer[BidimensionalPoint] = new CentroidInitializer[BidimensionalPoint] {
-    override def initialize[A](points: List[BidimensionalPoint[A]], number: Int)(implicit frac: Fractional[A]): List[Centroid[BidimensionalPoint, A]] = {
-      //@tailrec
-      def go(centroids: List[Centroid[BidimensionalPoint, A]], points: List[BidimensionalPoint[A]], remainingCentroids: Int): List[Centroid[BidimensionalPoint, A]] = {
-        if(remainingCentroids == 0)
-          centroids
-        else {
-          ???
-        }
-      }
-      /*
-      Choose one center uniformly at random from among the data points.
-      For each data point x, compute D(x), the distance between x and the nearest center that has already been chosen.
-      Choose one new data point at random as a new center, using a weighted probability distribution where a point x is chosen with probability proportional to D(x)2.
-       */
-
-      go(List.empty, points, number)
+  val unidimensionalRandomInitializer: CentroidInitializer[UnidimensionalPoint] = new CentroidInitializer[UnidimensionalPoint] {
+    override def initialize[A](points: List[UnidimensionalPoint[A]], number: Int)(implicit frac: Fractional[A]): List[Centroid[UnidimensionalPoint, A]] = {
+      (1 to number)
+        .map { p =>
+          Centroid("Centroid " + p, points(Random.nextInt(points.length)))
+        }.toList
     }
   }
 }
--- src/main/scala/data/kmeans/structures/CentroidCalculator.scala
+++ src/main/scala/data/kmeans/structures/CentroidCalculator.scala
@@ -1,6 +1,6 @@
 package data.kmeans.structures
 
-import common.data.BidimensionalPoint
+import common.data.{BidimensionalPoint, TridimensionalPoint, UnidimensionalPoint}
 
 import scala.language.higherKinds
 
@@ -14,7 +14,33 @@ object CentroidCalculator {
                                        points: List[BidimensionalPoint[A]])(implicit frac: Fractional[A]): Centroid[BidimensionalPoint, A] = {
       val centroidCoordinates = points.reduce((p, c) => BidimensionalPoint(centroid.name, frac.plus(p.X, c.X), frac.plus(p.Y, c.Y)))
 
+      //TODO find a better name
       Centroid(centroid.name, BidimensionalPoint("?????", frac.div(centroidCoordinates.X, frac.fromInt(points.length)), frac.div(centroidCoordinates.Y, frac.fromInt(points.length))))
+    }
+  }
+
+  implicit val tridimensionalPointImplicit: CentroidCalculator[TridimensionalPoint] = new CentroidCalculator[TridimensionalPoint] {
+    override def repositionCentroid[A](centroid: Centroid[TridimensionalPoint, A], points: List[TridimensionalPoint[A]])(implicit frac: Fractional[A]): Centroid[TridimensionalPoint, A] = {
+      val centroidCoordinates = points.reduce { (p, c) =>
+        TridimensionalPoint(centroid.name,
+          frac.plus(p.X, c.X), frac.plus(p.Y, c.Y), frac.plus(p.Z, c.Z))
+      }
+
+      //TODO find a better name
+      Centroid(centroid.name,
+        TridimensionalPoint("?????",
+          frac.div(centroidCoordinates.X, frac.fromInt(points.length)),
+          frac.div(centroidCoordinates.Y, frac.fromInt(points.length)),
+          frac.div(centroidCoordinates.Z, frac.fromInt(points.length))))
+    }
+  }
+
+  implicit val unidimensionalPointImplicit: CentroidCalculator[UnidimensionalPoint] = new CentroidCalculator[UnidimensionalPoint] {
+    override def repositionCentroid[A](centroid: Centroid[UnidimensionalPoint, A], points: List[UnidimensionalPoint[A]])(implicit frac: Fractional[A]): Centroid[UnidimensionalPoint, A] = {
+      val centroidCoordinates = points.reduce((p, c) => UnidimensionalPoint(centroid.name, frac.plus(p.X, c.X)))
+
+      //TODO find a better name
+      Centroid(centroid.name, UnidimensionalPoint("?????", frac.div(centroidCoordinates.X, frac.fromInt(points.length))))
     }
   }
 }
